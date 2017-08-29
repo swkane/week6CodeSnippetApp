aa5a448e4d3a7911c0114212313417b640d13d19
Sam <swkane@indiana.edu>
2017-08-29 19:11:40 -0400

    complete user registration

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express();
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const Snippet = require('./models/snippet');
-const addSnippetController = require('./controllers/add');
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
+const addSnippetController = require('./controllers/add-snippet');
+const session = require('express-session');
 // const editSnippetController = require('./controllers/edit-snippet');
 
 
 // Mongoose / Mongo Boiler Plate
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-const url = 'mongodb://localhost:27017/newsnippetdb';
+const url = 'mongodb://localhost:27017/snippetdb';
 mongoose.connect(url);
 const MongoClient = require('mongodb').MongoClient;
 
@@ -26,6 +29,13 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
 // Controller Routes
 app.use('/add', addSnippetController);
 // app.use('/edit', editSnippetController);
@@ -34,6 +44,32 @@ app.use('/add', addSnippetController);
 
 app.get('/', function(req, res) {
   res.render('login');
+});
+
+// register route
+
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
+app.post('/register/complete', function(req, res) {
+  let username = req.body.username;
+  let passHash = bcrypt.hashSync(req.body.password, 8);
+  MongoClient.connect(url, function(err, db) {
+    var user = new User({username: username, password: passHash, snippets: []});
+    user.save()
+    .then(function() {
+      // actions to take on success
+      console.log("User Added Success");
+      console.log(user);
+      db.users.insertOne(user);
+    })
+    .catch(function() {
+      console.log("User Add Error");
+      // action to take on error
+    });
+    res.redirect('/');
+  });
 });
 
 app.get("/home", function(req ,res) {
@@ -49,7 +85,7 @@ app.get("/home", function(req ,res) {
 });
 
 app.get('/add', function(req, res) {
-  res.render('add');
+  res.render('add-snippet');
 });
 
 // !!Only run this code once to set up the db
--- readme.md
+++ readme.md
@@ -3,4 +3,3 @@ Todos
 -- build in the ability to register a new user
 -- build in authentication to check for valid user
 -- add snippets to a corresponding collection for each user
--- fix the controllers
